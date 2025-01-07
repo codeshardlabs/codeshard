ff278bead2aa3d88f902c8ef3a3b99c14513f74d
Mridul Dhiman <dhimanmridul91@gmail.com>
2025-01-07 17:01:59 +0530

    [feat] added user relations

--- src/db/schema/users.js
+++ src/db/schema/users.js
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 
 
@@ -6,3 +7,15 @@ export const users = pgTable({
     followerId: varchar(256),
     followingId: varchar(256),
 })
+
+export const usersRelations = relations(users, ({ one }) => ({
+	follower: one(users, {
+		fields: [users.followerId],
+		references: [users.id],
+    }),
+    following: one(users, {
+        fields: [users.followingId],
+        references: [users.id]
+    })
+}));
+
